263b0a4fdd448dee240567c75fd68f62cd7d67b2
Mrugesh Mohapatra <me@raisedadead.com>
2018-04-04 12:51:54 +0530

    chore(tools): add prettier and commit helpers
    ISSUES CLOSED: #29

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,21 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+/* eslint-env node */
+
+const { types, scopes, allowCustomScopes } = require("./commitizen.config");
+
+const validTypes = types.map(type => type.value);
+const validScopes = scopes.map(scope => scope.name);
+const scopeValidationLevel = allowCustomScopes ? 1 : 2;
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+
+  // Add your own rules. See http://marionebl.github.io/commitlint
+  rules: {
+    // Apply valid scopes and types
+    "scope-enum": [scopeValidationLevel, "always", validScopes],
+    "type-enum": [2, "always", validTypes],
+
+    // Disable language rule
+    lang: [0, "always", "eng"]
+  }
+};
