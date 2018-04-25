fe0182f9092d4ab166c697d6108354927c8e4700
Otto Jongerius <otto.jongerius@gmail.com>
2018-04-25 20:26:53 +1200

    chore(tooling): original source file and line number in stacktrace (#67)

---
+++ _webpack/include.js
@@ -0,0 +1,10 @@
+// Add source map support for proper stack traces
+import 'source-map-support/register';
+
+// Catch all unhandled exceptions and print their stack trace.
+// Required if the handler function is async, as promises
+// can swallow error messages.
+process.on('unhandledRejection', e => {
+  console.error(e.stack);
+  process.exit(1);
+});
