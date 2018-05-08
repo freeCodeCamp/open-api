40de4e31d1121dfa70519e91a495ccefc0613582
Otto Jongerius <otto.jongerius@gmail.com>
2018-05-08 12:48:49 +1200

    fix: update header generation script, add alias for yarn

--- scripts/generateHeader.js
+++ scripts/generateHeader.js
@@ -1,5 +1,7 @@
+const secrets = require('../src/config/secrets.js');
 const jwt = require('jsonwebtoken');
-const { JWT_CERT } = process.env;
+
+const JWT_CERT = secrets.getSecret().JWT_CERT;
 
 const token = jwt.sign({ id: 123, name: 'Charlie' }, JWT_CERT);
 const headers = '{"Authorization": "Bearer ' + token + '"}';
